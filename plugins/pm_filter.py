bae32584c69c0e3cc7dc66d906f907f8ec9aca4d
Emily <71280699+Emilyoftg@users.noreply.github.com>
2022-01-05 19:45:03 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -37,7 +37,7 @@ async def next_page(bot, query):
 
     ident, req, key, offset = query.data.split("_")
     if int(req) not in [query.from_user.id, 0]:
-        return await query.answer(f"⚠️ Hey, {query.from_user.first_name}! Search Your Own File, Don't Click Others Results 😬", show_alert=True)
+        return await query.answer(f"കൊള്ളാല്ലോ നീ 😱", show_alert=True)
     try:
         offset = int(offset)
     except:
